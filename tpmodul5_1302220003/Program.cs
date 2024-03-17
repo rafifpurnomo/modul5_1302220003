76cbfd1e067810649842312cb11d465d136fb241
rafif purnomo <rafif.purnomo@gmail.com>
2024-03-18 01:47:49 +0700

    menambahkan class HaloGeneric dengan method

--- tpmodul5_1302220003/Program.cs
+++ tpmodul5_1302220003/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine("Halo user " + nama);
+    }
+}
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("masukan nama anda: ");
+        string nama = Console.ReadLine();
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(nama);
     }
 }
