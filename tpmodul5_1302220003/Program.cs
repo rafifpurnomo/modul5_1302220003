06f2dcfecb6e5172472a83a9cd55ded8491cc8b2
rafif purnomo <rafif.purnomo@gmail.com>
2024-03-18 01:50:38 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220003/Program.cs
+++ tpmodul5_1302220003/Program.cs
@@ -1,8 +1,11 @@
-﻿public class HaloGeneric
+﻿public class DataGeneric<T>
 {
-    public void SapaUser<T>(T nama)
+    private T Data;
+    public DataGeneric(T data) => Data = data;
+
+    public void printData()
     {
-        Console.WriteLine("Halo user " + nama);
+        Console.WriteLine("data yang tersimpan :" + Data);
     }
 }
 
@@ -10,9 +13,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("masukan nama anda: ");
-        string nama = Console.ReadLine();
-        HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser(nama);
+        Console.WriteLine("masukan NIM anda");
+        string nim = Console.ReadLine();
+        DataGeneric<string> dataNim = new DataGeneric<string>(nim);
+        dataNim.printData();
     }
 }
