4544aae0c2c653e2f37bd0408596e096bb4594f3
rafif purnomo <rafif.purnomo@gmail.com>
2024-03-18 01:53:31 +0700

    merge generic-method & class

--- tpmodul5_1302220003/Program.cs
+++ tpmodul5_1302220003/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine("Halo user " + nama);
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T Data;
+    public DataGeneric(T data) => Data = data;
+
+    public void printData()
+    {
+        Console.WriteLine("data yang tersimpan :" + Data);
+    }
+}
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("masukan nama anda: ");
+        string nama = Console.ReadLine();
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(nama);
+
+        Console.WriteLine("masukan NIM anda");
+        string nim = Console.ReadLine();
+        DataGeneric<string> dataNim = new DataGeneric<string>(nim);
+        dataNim.printData();
     }
 }
